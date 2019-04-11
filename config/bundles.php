086ef9b9735f777356309f58249bce73359a8322
Julien Mercier-Rojas <devci@j3ck3l.me>
2019-04-11 21:32:24 +0200

    :wrench: Add profiler configuration files

--- config/bundles.php
+++ config/bundles.php
@@ -11,4 +11,5 @@ return [
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
 ];
