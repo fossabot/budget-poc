d6b5044558a46853da63bb8336563d25d5505e9c
Julien Mercier-Rojas <devci@j3ck3l.me>
2019-04-14 17:00:35 +0200

    :fire: Remove empty test

--- tests/unit/Service/SystemClockTest.php
+++ tests/unit/Service/SystemClockTest.php
@@ -8,15 +8,16 @@ namespace Test\App\Service;
 
 use App\Service\SystemClock;
 use Codeception\Test\Unit;
+use DateTimeImmutable;
 
 class SystemClockTest extends Unit
 {
     public function testNow()
     {
-        $now = new \DateTimeImmutable('now');
+        $now = new DateTimeImmutable('now');
         $service = new SystemClock();
         $systemClock = $service->now();
         $this->assertGreaterThanOrEqual($now, $systemClock);
-        $this->assertLessThanOrEqual(new \DateTimeImmutable('now'), $systemClock);
+        $this->assertLessThanOrEqual(new DateTimeImmutable('now'), $systemClock);
     }
 }
